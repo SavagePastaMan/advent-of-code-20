a444a3ed8b13fe6cc82d8daecaac8b567444630c
SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>
2020-12-03 13:11:25 -0500

    polished day 3

--- solutions/day03.py
+++ solutions/day03.py
@@ -1,7 +1,10 @@
 import helper
 
 data = helper.day(3)
+
+
 # print(data)
+
 
 def parse(raw):
     return raw.split("\n")
@@ -10,22 +13,28 @@ def parse(raw):
 data = parse(data)
 
 
-def part_one(right, down):
-    c = 0
-    y = 0
-    for row in data[::down]:
-        if row[y] == "#":
-            c += 1
-        y = (y + right) % len(row)
+# @helper.timer
+def part_one(slopes):
+    c = [0 for _ in slopes]
+    xs = [0 for _ in slopes]
+
+    for i, row in enumerate(data):
+        for n, (dx, dy) in enumerate(slopes):
+            if i % dy == 0:
+                if row[xs[n]] == "#":
+                    c[n] += 1
+                xs[n] = (xs[n] + dx) % len(row)
+
     return c
 
 
+@helper.timer
 def part_two():
-    return part_one(1, 1) * part_one(3, 1) * part_one(5, 1) * part_one(7, 1) * part_one(1, 2)
+    return helper.prod(part_one([(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]))
 
 
-# print(part_one(3, 1))
+# print(part_one([(3, 1)]))
 print(part_two())
 
 # helper.submit(3, part_one)
-helper.submit(3, part_two)
+# helper.submit(3, part_two)
