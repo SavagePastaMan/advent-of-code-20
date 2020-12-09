81728aa1faa8dad1d66c4b165c8258496e33199a
SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>
2020-12-08 23:49:12 -0500

    refactor day 8 to prepare for another appearance of the bootloader

--- solutions/day08.py
+++ solutions/day08.py
@@ -1,80 +1,115 @@
+from __future__ import annotations
+
 import helper
 
-data = helper.day(8)
+from dataclasses import dataclass
+from typing import Tuple, List
+from enum import Enum
 
 
-# print(data)
-
-def parse(raw):
-    L = []
-    for x in raw.split("\n"):
-        a, b = x.split()
-        L.append((a, int(b)))
-
-    return L
+class Operation(Enum):
+    acc = "acc"
+    jmp = "jmp"
+    nop = "nop"
 
 
-data = parse(data)
-# print(data)
+@dataclass(frozen=True)
+class Instruction:
+    op: Operation
+    arg: int
 
-def part_one():
-    v = set()
-    acc = 0
-    i = 0
-    while True:
-        if i in v:
-            return acc
-        v.add(i)
-        if data[i][0] == "acc":
-            acc += data[i][1]
-            i += 1
-        elif data[i][0] == "jmp":
-            i += data[i][1]
+    @classmethod
+    def from_string(cls, string: str) -> Instruction:
+        op, arg = string.split(" ")
+        return cls(Operation(op), int(arg))
+
+    def unpack(self) -> Tuple[Operation, int]:
+        return self.op, self.arg
+
+    def __str__(self):
+        return str((self.op, self.arg))
+
+
+@dataclass
+class Application:
+    instructions: List[Instruction]
+    accumulator: int = 0
+
+    def __enter__(self) -> Application:
+        self._original_instructions = self.instructions.copy()
+        self._original_accumulator = self.accumulator
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.instructions = self._original_instructions
+        self.accumulator = self._original_accumulator
+
+    @classmethod
+    def from_lines(cls, lines: List[str]):
+        return cls(
+            [Instruction.from_string(line) for line in lines]
+        )
+
+    def run(self) -> int:
+        visited = set()
+        pointer = 0
+
+        while 0 <= pointer < len(self.instructions):
+            inst = self.instructions[pointer]
+            op, arg = inst.unpack()
+
+            if pointer in visited:
+                break
+            visited.add(pointer)
+
+            if op is Operation.acc:
+                self.accumulator += arg
+                pointer += 1
+            elif op is Operation.jmp:
+                pointer += arg
+            else:
+                pointer += 1
+
+        return pointer
+
+    def __str__(self):
+        return "\n".join(map(str, self.instructions))
+
+
+def parse() -> Application:
+    data = helper.day(8)
+
+    return Application.from_lines(data.splitlines())
+
+
+def part_one(app: Application) -> int:
+    with app:
+        app.run()
+        return app.accumulator
+
+
+def part_two(app: Application) -> int:
+    for i, inst in enumerate(app.instructions):
+        op, arg = inst.unpack()
+
+        if op is Operation.jmp:
+            op = Operation.nop
+        elif op is Operation.nop:
+            op = Operation.jmp
         else:
-            i += 1
+            continue
+
+        with app:
+            app.instructions[i] = Instruction(op, arg)
+            pointer = app.run()
+
+            if pointer == len(app.instructions):
+                return app.accumulator
 
 
-def is_infinite():
-    v = set()
-    acc = 0
-    i = 0
-    while len(v) != len(data):
-        if i in v:
-            return False
-        v.add(i)
+if __name__ == '__main__':
+    data = helper.day(8)
+    app = Application.from_lines(data.splitlines())
 
-        if i == len(data):
-            return acc
-
-        if data[i][0] == "acc":
-            acc += data[i][1]
-            i += 1
-        elif data[i][0] == "jmp":
-            i += data[i][1]
-        else:
-            i += 1
-
-    return acc
-
-
-def part_two():
-    for i, x in enumerate(data):
-        if x[0] == "nop":
-            t = data[i]
-            data[i] = ("jmp", x[1])
-            if c := is_infinite():
-                return c
-            data[i] = t
-        elif x[0] == "jmp":
-            t = data[i]
-            data[i] = ("nop", x[1])
-            if c := is_infinite():
-                return c
-            data[i] = t
-
-
-print(part_one())
-print(part_two())
-
-helper.submit(8, part_one)
-helper.submit(8, part_two)
+    print(part_one(app))
+    print(part_two(app))
